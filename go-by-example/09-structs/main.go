ead705edaedf638d8f1833e1deef46c12acc8771
luna <paulkingdev7@gmail.com>
2024-12-26 01:36:53 +0300

    #structs

---
+++ go-by-example/09-structs/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type USER struct {
+	Name  string
+	Email string
+	Age   int
+}
+
+func main() {
+	ron := USER{"Rob", "rob@gmail.com", 67}
+	fmt.Printf("%+v\n", ron)
+
+	// the new keyword : it only allocates but it doesnt initialize memory
+	/**
+	. In golang NEW keyword is used to allocate memory for a value of specified type and returns a pointer to it
+	. User creates a pointer to an instance of USER. It allocates memory for the struct but does not initialize it
+	**/
+	var SAM = new(USER)
+	SAM.Name = "Samwel"
+	SAM.Email = "mwaniki@tajiri.com"
+	SAM.Age = 25
+	fmt.Printf("%+v\n", SAM)
+
+	var TOBY = &USER{"Toby", "toby@tajiri.com", 45}
+	fmt.Printf("%+v\n", TOBY)
+
+	fmt.Println("Structs baby!")
+}
+
+/**
+. In structs we use other data-types to create our own data type
+**/
