da7d2dfd8275650aa1a6b2ef97b01c1d8bf0f2ee
luna <paulkingdev7@gmail.com>
2024-12-26 00:42:38 +0300

    #slices

--- go-by-example/07-slices/main.go
+++ go-by-example/07-slices/main.go
@@ -1,54 +1,54 @@
-/**
-
-SAMPLE PROGRAM THAT TAKES USER INPUT, STORES IN A SLICE AND LOOPS OVER THE SLICE IN OUTPUT
-
-
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"slices"
+	"sort"
 )
 
 func main() {
-	var longIn = []string{"Hello", "World"}
-	fmt.Println()
-	fmt.Println("NB: To Understand slices, first understand arrays")
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter your name")
-	input, _ := reader.ReadString('\n')
-	fmt.Println(input)
-	longIn = append(longIn, input)
-	for _, value := range longIn {
-		fmt.Println(value)
-	}
-	fmt.Println("I know how to take user input in golang")
+	var names = []string{"Tuwei", "Marion", "Chepkoech"}
+
+	fmt.Println("These are the names", names)
+
+	names = append(names, "Mbugua")
+
+	fmt.Println("These are the new names", names)
+
+	names = append(names[1:3]) // This is how we do a slice
+	fmt.Println(names)
+	heros := make([]string, 3, 3)
+	/**
+	This is how we create a slice, type,length, capacity
+	The legth and capacity does not matter since go recognizes that its a string and it will not be capped!
+
+	When we make() a slice, it allocates/creates an array with length  and capacity but returns a slice.
+	**/
+	heros[0] = "Scarlet"
+	heros[1] = "DeadPool"
+	heros[2] = "Human-Torch"
+	fmt.Println(heros)
+	heros = append(heros, "Wolverine")
+	fmt.Println(heros)
+
+	/**
+	Sorting is good to do
+	. Copy the excisting slice to a new slice and work with the new slice
+	**/
+
+	numbers := []int{3, 56, 2, 9, 2}
+	isSorted := sort.IntsAreSorted(numbers)
+	fmt.Println(isSorted)
+	SORTED := make([]int, len(numbers))
+	copy(SORTED, numbers)
+
+	// Method 1
+	sort.Ints(SORTED)
+	fmt.Println("This is the sorted slice using method 1", SORTED)
+
+	// Method 2
+	METHOD_2 := make([]int, len(numbers))
+	copy(METHOD_2, numbers)
+	slices.Sort(METHOD_2)
+	fmt.Println("This is the sorted slice using method 2", METHOD_2)
 }
-
-**/
-
-/**
-1. An array has a length and element type
-
-ie var name [4]string
-type = string
-lenght = 4
-
-2. A dedicatedtslice has the following specification
-[]T
-T = type
-
-3. A slice can be created using
-	. letters := []string {"a"}
-	. var letters []string
-	. func make([]T, len, cap) []T
-	=> When we use the make, it takes a type, length and an optional capacity. It allocates an array and returns a slice that refers to that array!
-
-	=> The make() can be used to create both slices and maps
-
-
-
-
-
-**/
